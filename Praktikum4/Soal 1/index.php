f57c22c039c067c00c270704022e1a9253d13382
HendryJoellyo <90808529+HendryJoellyo@users.noreply.github.com>
2021-10-18 18:53:12 +0700

    Add files via upload

---
+++ Praktikum4/Soal 1/index.php
@@ -0,0 +1,32 @@
+<html>
+    <head>
+        <meta name="author" content="Hendry Joellyo Pattiasina 2172038">
+        <title>Soal 1</title>
+</head>
+<body>
+    <center>
+        <h4>Form input</h4>
+        <form action="Hasil.php" name="forminput" method="post">
+        <p>
+        <label>Nama :</label>
+            <input type="text" name="nama"> <br>
+            </p>
+            <p>
+            <label>Email :</label>
+            <input type="text" name="email"> <br>
+            </p>
+            <p>
+            <label>Phone Number :</label>
+            <input type="text" name="telphone"> <br>
+            </p>
+            <p>
+            <label>Hobby :</label>
+            <input type="text" name="hobi"> <br>
+            </p>
+            Description : <textarea name="desk" id="" cols="30" rows="10"></textarea> <br>
+            <input type="Submit" value="Submit">
+            <input type="Reset" value="Cancel">
+</center>
+</form>
+<body>
+    </html>
---
+++ Praktikum4/Soal 2/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="author" content="Hendry Joellyo Pattiasina 2172038">
+    <title>Soal 2</title>
+</head>
+<body>
+
+    <form action="Hasil.php" method="get">
+
+        <table border="1" align="center">
+            <tr>
+                <td colspan="2" align="center" bgcolor="red">
+                    Kalkuator
+                </td>
+            </tr>
+            <tr>
+                <td bgcolor="pink">Angka pertama</td>
+                <td><input type="number" name="angka1" id="angka1"></td>
+            </tr>
+            <tr>
+                <td bgcolor="pink">Angka kedua</td>
+                <td><input type="number" name="angka2" id="angka2"></td>
+            </tr>
+            <tr>
+            <tr>
+                <td bgcolor="pink">Operator</td>
+                <td>
+                    <input type="text" name="operator" id="operator"> 
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2" align="center" bgcolor="red">
+                 <input type="submit" value="submit">
+                 <input type="reset" value="reset">
+                </td>
+            </tr>
+        </table>
+
+    </form>  
+    
+</body>
+</html>
